318c688dc17d685e80679ce7544faaa0767c5ea1
alexgrauroca <alexgrauroca91@gmail.com>
2025-06-06 14:49:48 +0200

    Second step

--- Exercises/15-async/main.go
+++ Exercises/15-async/main.go
@@ -17,6 +17,34 @@ func main() {
 	}()
 	fmt.Println("goroutine triggered")
 	wg.Wait()
+
+	parallelFuncs := []struct {
+		name     string
+		delaySec float32
+	}{
+		{
+			name:     "C",
+			delaySec: 3,
+		},
+		{
+			name:     "B",
+			delaySec: 2,
+		},
+		{
+			name:     "A",
+			delaySec: 1,
+		},
+	}
+
+	for _, parallelFunc := range parallelFuncs {
+		wg.Add(1)
+		go func() {
+			asyncFunc(parallelFunc.delaySec, parallelFunc.name)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	asyncFunc(1, "D")
 }
 
 func asyncFunc(delaySec float32, name string) {
